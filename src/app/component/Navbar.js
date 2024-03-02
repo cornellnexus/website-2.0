44e14382cc87332939baf8a391b73be2bcf4f572
Michael Wu <michaelwu2004@gmail.com>
2024-03-02 14:43:49 -0500

    Add page and navbar component

---
+++ src/app/component/Navbar.js
@@ -0,0 +1,14 @@
+
+
+export default function Navbar() {
+  return (
+    <div className="nav-links">
+      <a href="">Home</a>
+      <a href="/about">About</a>
+      <a href="">Team</a>
+      <a href="">Support</a>
+      <a href="">Apply</a>
+      <a href="">Contact</a>
+    </div>
+  )
+}
