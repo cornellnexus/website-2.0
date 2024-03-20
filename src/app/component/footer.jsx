1b07002e53096c893aa73915c392c162e2cfedb0
Luke Tao <lyt5@cornell.edu>
2024-03-20 17:03:11 -0400

    Improve footer appearance on very small screens

--- src/app/component/footer.jsx
+++ src/app/component/footer.jsx
@@ -2,44 +2,48 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="bg-white flex flex-wrap items-center justify-around md:justify-between px-12 py-4">
-      <div className="flex gap-6 items-center">
-        <a href="https://www.instagram.com/cornellnexus/" target="_blank">
-          <Image
-            id="insta-icon"
-            src={"/insta-svg.svg"}
-            width="40"
-            height="40"
-            alt="Instagram"
-          />
-        </a>
-        <a href="https://github.com/cornellnexus" target="_blank">
-          <Image
-            id="github-icon"
-            src={"/github-svg.svg"}
-            width="40"
-            height="40"
-            alt="Github"
-          />
-        </a>
-        <a href="https://www.facebook.com/cornellnexus/" target="_blank">
-          <Image
-            id="facebook-icon"
-            src={"facebook-svg.svg"}
-            width="40"
-            height="40"
-            alt="Facebook"
-          />
-        </a>
-        <a href="mailto:nexus@cornell.edu" target="_blank">
-          <Image
-            id="email-icon"
-            src={"email-svg.svg"}
-            width="40"
-            height="40"
-            alt="E-mail"
-          />
-        </a>
+    <footer className="bg-white flex flex-wrap items-center gap-4 justify-around sm:justify-between px-12 py-4">
+      <div className="flex flex-wrap gap-6 justify-center items-center">
+        <div className="flex flex-row justify-between gap-6">
+          <a href="https://www.instagram.com/cornellnexus/" target="_blank">
+            <Image
+              id="insta-icon"
+              src={"/insta-svg.svg"}
+              width="30"
+              height="30"
+              alt="Instagram"
+            />
+          </a>
+          <a href="https://www.facebook.com/cornellnexus/" target="_blank">
+            <Image
+              id="facebook-icon"
+              src={"facebook-svg.svg"}
+              width="30"
+              height="30"
+              alt="Facebook"
+            />
+          </a>
+        </div>
+        <div className="flex flex-row justify-between gap-6">
+          <a href="https://github.com/cornellnexus" target="_blank">
+            <Image
+              id="github-icon"
+              src={"/github-svg.svg"}
+              width="30"
+              height="30"
+              alt="Github"
+            />
+          </a>
+          <a href="mailto:nexus@cornell.edu" target="_blank">
+            <Image
+              id="email-icon"
+              src={"email-svg.svg"}
+              width="30"
+              height="30"
+              alt="E-mail"
+            />
+          </a>
+        </div>
       </div>
       <div className="text-2xl text-center">
         <p>Nexus at Cornell © 2024</p>
