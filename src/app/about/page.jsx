f8294ac327e87d009e0fa044254bff0ac3405615
Vinny <vh222@cornell.edu>
2024-03-30 21:36:20 -0400

    Added animations and improved carousel latency
    + added animations for different text and images on the page
    - removed quality={100} for the carousel because the image quality is negligible if we have good quality photos and there is significant latency attributed to preserving 100% quality

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function AboutUs() {
   const mission =
@@ -9,7 +10,7 @@ export default function AboutUs() {
   const progressDesc =
     "We are currently in the process of developing the first iteration of our robot, and our team is diligently working towards its completion. We are optimistic about our progress and are striving to finish the project by the end of this year. Our dedicated efforts and commitment to innovation are driving us towards achieving this milestone, and we are excited about the possibilities that lie ahead.";
   const storyDesc =
-    "Starting off with just 6 original members, Nexus grew to become a team of 30+ talented and passionate individuals who want to help make the world a better place. We hope to increase our enthusiasm for people, product, and innovation through a collaborative environment within our team.";
+    "Starting off with just 6 original members — united under a single mission towards using technology for sustainability — Nexus grew to become a team of 30+ talented and passionate individuals who want to help make the world a better place. We hope to increase our enthusiasm for people, product, and innovation through a collaborative environment within our team.";
   const [index, setIndex] = useState(0);
   const images = ["/robot.png", "/mechanical.jpeg"];
 
@@ -25,11 +26,22 @@ export default function AboutUs() {
     <>
       {/* Main page content */}
       <div id="ourmission" className="bg-grey space-y-5 px-4">
-        <div className="flex flex-col items-center pb-10">
-          <div className="text-center title-size font-semibold text-5xl my-10">
+        <div className="flex flex-col items-center pb-12">
+          <motion.div
+            initial={{y: -23, opacity: 0}}
+            whileInView={{y:[-23, -6, 0], opacity: [0,.4,1], transition: {delay: .15, duration: .6, times: [0, 0.5, 1]}}}
+            viewport={{once: true}}
+            className="text-center title-size font-semibold text-5xl my-10"
+          >
             Our Mission
-          </div>
-          <div id="carousel" className="flex flex-row justify-center mb-5">
+          </motion.div>
+          <motion.div 
+            id="carousel" 
+            className="flex flex-row justify-center mb-6"
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1, transition: {delay: .15, duration: .6, ease: "easeOut"}}}
+            viewport={{once: true}}
+          >
             <button
               onClick={handlePrev}
               id="prevBtn"
@@ -42,8 +54,7 @@ export default function AboutUs() {
                 className="object-cover"
                 id="carouselImage"
                 src={images[index]}
-                alt="progressimage"
-                quality={100}
+                alt="progress image"
                 fill={true}
               />
             </div>
@@ -54,16 +65,28 @@ export default function AboutUs() {
             >
               &gt;
             </button>
-          </div>
-          <div className="max-w-screen-md text-center">{mission}</div>
+          </motion.div>
+          <motion.div 
+            className="max-w-screen-md text-center"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0, transition: {type: "spring", duration: .8, bounce: .25}}}
+            viewport={{ once: true, amount: .7 }}
+          >
+            {mission}
+          </motion.div>
         </div>
       </div>
       <div
         id="story"
         className="flex flex-col items-center text-white px-4 py-8 bg-teal"
       >
-        <div className="flex justify-center flex-wrap flex-row gap-12 mb-12">
-          <div>
+        <div className="flex justify-center flex-wrap flex-row gap-12 mt-12 mb-12">
+          <motion.div
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{duration: .8, ease:"easeOut"}}
+            viewport={{once: true, amount: .3}}
+          >
             <Image
               className="rounded shadow"
               src={"/progress.png"}
@@ -71,15 +94,26 @@ export default function AboutUs() {
               width="400"
               height="400"
             />
-          </div>
-          <div className="max-w-sm">
+          </motion.div>
+          <motion.div 
+            initial={{opacity: 0, x: 50}}
+            whileInView={{opacity: 1, x: 0}}
+            transition={{duration: .8, ease:"easeOut"}}
+            viewport={{once: true, amount: .7}}
+            className="max-w-sm"
+          >
             <p className="text-4xl font-medium">Progress</p>
             <p>{progressDesc}</p>
-          </div>
+          </motion.div>
         </div>
 
         <div className="flex justify-center flex-wrap flex-row-reverse gap-12 mb-12">
-          <div>
+          <motion.div
+            initial={{opacity: 0}}
+            whileInView={{opacity: 1}}
+            transition={{duration: .8, ease:"easeOut"}}
+            viewport={{once:true, amount:.3}}
+          >
             <Image
               className="rounded shadow"
               src={"/story.png"}
@@ -87,11 +121,17 @@ export default function AboutUs() {
               width="400"
               height="400"
             />
-          </div>
-          <div className="max-w-sm">
+          </motion.div>
+          <motion.div 
+            initial={{x:-50, opacity:0}}
+            whileInView={{x:0, opacity:1}}
+            transition={{duration: .8, ease:"easeOut"}}
+            viewport={{once: true, amount: .7}}
+            className="max-w-sm"
+          >
             <p className="text-4xl font-medium">Story</p>
             <p>{storyDesc}</p>
-          </div>
+          </motion.div>
         </div>
       </div>
     </>
