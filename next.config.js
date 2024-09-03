34fcc8aa987f0c24001c812b07139ae242a2c428
Vinny <vh222@cornell.edu>
2024-09-03 18:11:19 -0400

    Added redirect from /recruitment to /apply

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/recruitment',
+        destination: '/apply',
+        permanent: true,
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
